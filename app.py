51c5d1f5c63c31e778cb2fb151cabd8d8577d973
Destiny911gtr <dhanush911gtr@gmail.com>
2025-08-09 00:54:48 +0530

    Fly.io compatibility

--- app.py
+++ app.py
@@ -301,4 +301,5 @@ def server_error(e):
     return jsonify(error='Internal Server Error'), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
